a887f5fdbe1f562863638e7279038dd749640dfe
Ray Chen <ray@raychen.io>
2019-11-10 17:06:04 +0800

    Tidy up paths for bootstrap script, support .ctags

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@
 
 # Setup symlinks
 cd ~/
-ln -s dotfiles/.zshrc .zshrc
-ln -s dotfiles/.gitconfig .gitconfig
-ln -s dotfiles/.gitignore_global .gitignore_global
-ln -s dotfiles/.flake8 .flake8
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.flake8 ~/.flake8
+mkdir -p ~/.ctags.d; ln -s ~/dotfiles/.ctags ~/.ctags.d/default.ctags
