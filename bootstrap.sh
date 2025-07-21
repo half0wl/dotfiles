e4e36b746fd531b875b4fa1871df6080c3ce5c29
Ray Chen <ray@raychen.io>
2025-07-21 21:08:23 +0800

    claude: Add claude-settings.json

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,4 @@ ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.flake8 ~/.flake8
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/claude-settings.json ~/.claude/settings.json
