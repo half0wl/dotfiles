051fd4990adb74c1ee058d3a53d5b084419699a5
Ray Chen <ray@raychen.io>
2026-01-08 23:50:53 +0800

    nvim: Dependency updates

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-  { "windwp/nvim-spectre", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "folke/todo-comments", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -57,7 +57,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "typescript-language-server" })
